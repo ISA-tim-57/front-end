72bced299b63d54bc6d0acc09c1e6fd058906162
luka-rakin <116868040+luka-rakin@users.noreply.github.com>
2023-12-20 15:30:57 +0100

    Design update

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/modules/student3/model/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(
+    private router : Router,
+    private authService : AuthService,
+  ){}
+
+  isLoggedIn(){
+    let user : User | null = this.authService.getUser();
+    if(user === null){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
+  goToLogin(){
+    this.isLoggedIn()
+    this.router.navigate(['login']);
+  }
+
+  logOut(){
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
 }
